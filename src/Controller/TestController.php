efb8a27b152977c89016d441569985a0962e0413
cassi <biagioli.cassandra@gmail.com>
2019-02-25 17:09:46 +0100

    debug testController page /

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,73 +4,73 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Movi;
+use App\Entity\Movie;
 use App\Entity\Evaluation;
-use Symfony\Component\HttpFoundation\Request
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 
 class TestController extends AbstractController
 {
-
-
     /**
      * fonction fète pr tester ds trucs
      * @Route("/test", name="test")
      */
-    public function test()
-    {
-        $ms = $this->getDoctrine()->getRepository(Movie::class)->findAll();
-        //fonction qui essé de calc moyen note flm mais prblm
-        for ($i=0; $i < count($ms) ; $i) {
-          $notes = $ms[$i]->getEvaluations()->getGrade();
-        }
-        return $this->render('test/index.html.twig', [
-          "ms" => $ms
-        ]);
-    }
+    // public function test()
+    // {
+    //     $ms = $this->getDoctrine()->getRepository(Movie::class)->findAll();
+    //     //fonction qui essé de calc moyen note flm mais prblm
+    //     for ($i=0; $i < count($ms) ; $i) {
+    //       $notes = $ms[$i]->getEvaluations()->getGrade();
+    //     }
+    //     return $this->render('test/index.html.twig', [
+    //       "ms" => $ms
+    //     ]);
+    // }
 
     /**
      * @Route("/", name="index")
      */
     public function index()
     {
-        $ms = $this->getDoctrine()->getRepository(Movie::class)->findAll;
+        $movies = $this->getDoctrine()->getRepository(Movie::class)->findAll();
         return $this->render('test/index.html.twig', [
-          "ms" => $ms
+          "movies" => $movies
         ]);
     }
 
     /**
-     * @Route("/single/{id}", name="index")
+     * @Route("/single/{id}", name="single")
      */
-    public function show(Movi $a)
+    public function show(Movie $movie)
     {
-        return $this->render('single.html.twig' [
-          "a" => $a
+        return $this->render('test/single.html.twig', [
+          "movie" => $movie
         ]);
     }
 
     /**
-     * @Route("/evaluation/{id}", name="index")
-     * @Isgranted("ROLE")
+     * @Route("/evaluation/{id}", name="evaluation")
+     * @IsGranted("ROLE")
      */
-    public function rate(Movi $b, Request $c)
+    public function rate(Movie $movie, User $user, Request $c)
     {
-        $d = new Evaluation();
+        $rate = new Evaluation();
 
-        $form = $this->createFormBuilder($d)
+        $form = $this->createFormBuilder($rate)
             ->add('comment')
             ->add('grade')
-            ->add('save', SubmitType::class)
+            ->add('sauvegarder', SubmitType::class)
             ->getForm();
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-          $d.setMovie($b);
-          $d.setUser($u);
+          $rate.setMovie($movie);
+          $rate.setUser($user);
           $entityManager = $this->getDoctrine()->getManager();
-          $entityManager->persist($d);
+          $entityManager->persist($rate);
           $entityManager->flush();
         }
 
